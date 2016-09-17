6eea5c65dc5781b04c6c0a26f93c3f2553b2a800
xingxingyang6 <1006382773@qq.com>
2016-09-17 11:23:29 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{//本程序在原始程序的基础上增加了一个计算0到9连续相加总和，并在输出hello的基础上输出和sum的值
 	int sum=0;
 	for(int i=0;i<10;i++){
 		sum=sum+i;
-	}
-	cout << "Hello World!" << endl;
-	cout << "The sum is :"<<sum << endl;
+	}//利用for循环计算sum
+	cout << "Hello World!" << endl;//输出Hello World!
+	cout << "The sum is :"<<sum << endl;//输出sum
 	return 0;
 }
