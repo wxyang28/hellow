ac13524d84449ee74b800ad508703d892cced03d
lgselite <1926917191@qq.com>
2016-10-09 17:11:59 +0800

    Add files via upload

---
+++ grade.java
@@ -0,0 +1,25 @@
+package com.lgs.bean;
+
+public class grade {
+     String username;
+     String chengji;
+     String time;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getChengji() {
+		return chengji;
+	}
+	public void setChengji(String chengji) {
+		this.chengji = chengji;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
