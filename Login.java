726014cfd3b4450d8364007b49d96635ba320a99
lgselite <1926917191@qq.com>
2016-10-11 18:50:46 +0800

    数据库对象
    :fish:

---
+++ Login.java
@@ -0,0 +1,26 @@
+package com.lgs.bean;
+
+public class Login {
+     private String user_name;
+     private String user_key;
+     private String role;
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_key() {
+		return user_key;
+	}
+	public void setUser_key(String user_key) {
+		this.user_key = user_key;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+     
+}
